71692c5285e129cde1742ca66c2f84880929119d
monikakucerova <monika-kucerova@outlook.cz>
2024-09-24 20:58:04 +0200

    Initial commit of DT: Noc vedcu to web

---
+++ exp_nocVedcu/soundSel.js
@@ -0,0 +1,761 @@
+var soundSel = [
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "ice"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "hen"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "bedLion-helmet",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "bat-blanket",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "monster-windowLion",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-frog",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "berryHen-catHen",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "hen-cakeHen",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "horse-dog",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "ice_lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "hen_lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "bedOak-pear",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "apple-branches",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "fire-oaktree",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion_ice"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion_hen"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "berryIce-jacket",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "jen-hatIce",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni1",
+    "speaker": "random",
+    "pair": "picnic-windowIce",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "ice"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "hen"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "bedLion-helmet",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "bat-blanket",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "monster-windowLion",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "lion",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-frog",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "berryHen-catHen",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "hen-cakeHen",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "horse-dog",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "ice_lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "hen_lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "bedOak-pear",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "apple-branches",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "oak",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "fire-oaktree",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion_ice"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion_hen"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "berryIce-jacket",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "jen-hatIce",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "ice",
+    "cond": "uni2",
+    "speaker": "random",
+    "pair": "picnic-windowIce",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "ice"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "hen"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "bedLion-helmet",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "bat-blanket",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "monster-windowLion",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "lion",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-frog",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "hen",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "hen",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "hen",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "berryHen-catHen",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "hen-cakeHen",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "hen",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "horse-dog",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "oak",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "ice_lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "hen_lion"
+  },
+  {
+    "vid": "oak",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp"
+  },
+  {
+    "vid": "oak",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "bedOak-pear",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "oak",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "apple-branches",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "oak",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "fire-oaktree",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "ice",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "bedLion-bat",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion"
+  },
+  {
+    "vid": "ice",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-berryIce",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion_ice"
+  },
+  {
+    "vid": "ice",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "barry-berryHen",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "lion_hen"
+  },
+  {
+    "vid": "ice",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "gavin-kevin",
+    "heard": "random",
+    "status": "mp",
+    "prereq": "oak"
+  },
+  {
+    "vid": "ice",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "berryIce-jacket",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "ice",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "jen-hatIce",
+    "heard": "random",
+    "status": "train"
+  },
+  {
+    "vid": "ice",
+    "cond": "bi",
+    "speaker": "random",
+    "pair": "picnic-windowIce",
+    "heard": "random",
+    "status": "fam"
+  },
+  {
+    "vid": "mouse",
+    "cond": "test",
+    "speaker": "randomTest",
+    "pair": "cakeMouse-cheese",
+    "heard": "randomTest",
+    "status": "fam"
+  },
+  {
+    "vid": "mouse",
+    "cond": "test",
+    "speaker": "randomTest",
+    "pair": "carrot-cookies",
+    "heard": "randomTest",
+    "status": "train"
+  },
+  {
+    "vid": "mouse",
+    "cond": "test",
+    "speaker": "randomTest",
+    "pair": "hatMouse-catMouse",
+    "heard": "randomTest",
+    "status": "train"
+  },
+  {
+    "vid": "mouse",
+    "cond": "test",
+    "speaker": "randomTest",
+    "pair": "brad-bread",
+    "heard": "randomTest",
+    "status": "mp"
+  },
+  {
+    "vid": "mouse",
+    "cond": "test",
+    "speaker": "randomTest",
+    "pair": "jan-jen",
+    "heard": "randomTest",
+    "status": "mp"
+  },
+  {
+    "vid": "mouse",
+    "cond": "test",
+    "speaker": "randomTest",
+    "pair": "barry-berryIce",
+    "heard": "randomTest",
+    "status": "mp"
+  },
+];
